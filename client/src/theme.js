b185ce28723d5fda93d54ca888cb91a78a271601
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-07-26 19:01:00 -0400

    add styled components

---
+++ client/src/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    black: "#111",
+    white: "rgb(250, 250, 250)",
+    blue: "#00408a",
+    gray: "#ddd"
+  },
+  breakpoints: {
+    notSmall: "(min-width: 30em)",
+    medium: "(min-width: 30em) and (max-width: 60em)",
+    large: "(min-width: 60em)"
+  }
+};
+
+export default theme;
