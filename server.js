f03a9e74c81ac7e855aa50cc78fb1eacb9098fba
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-07-26 19:00:24 -0400

    check when repo was updated on github

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const path = require("path");
+const fetch = require("node-fetch");
+const moment = require("moment");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const accessToken = process.env.GITHUB_TOKEN;
+
 // API calls
 app.get("/api/:user/:repo", (req, res) => {
-  console.log(req.params);
-  console.log("request");
-  res.send(req.params);
+  const query = `
+  query {
+    repository(owner:"${req.params.user}", name:"${req.params.repo}") {
+      updatedAt
+    }
+  }`;
+
+  fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => {
+      const rawUpdatedAt = json.data.repository.updatedAt;
+      const processedUpdatedAt = { updatedAt: moment(rawUpdatedAt).fromNow() };
+      res.send(processedUpdatedAt);
+    })
+    .catch(error => console.error(error));
 });
 
 if (process.env.NODE_ENV === "production") {
